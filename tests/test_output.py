2aeaf3e287a7d53f453a0983f88b5b13875de5d3
Łukasz Rogalski <rogalski.91@gmail.com>
2016-11-28 20:04:45 +0100

    adaptive banner width

---
+++ tests/test_output.py
@@ -0,0 +1,12 @@
+from unittest import mock
+from concentration import output
+
+
+def test_banner():
+    some_height = 24
+    with mock.patch('shutil.get_terminal_size', return_value=(12, some_height)):
+        assert output.banner('ABCD') == '####ABCD####'
+    with mock.patch('shutil.get_terminal_size', return_value=(24, some_height)):
+        assert output.banner('ABCD') == '##########ABCD##########'
+    with mock.patch('shutil.get_terminal_size', return_value=(2, some_height)):
+        assert output.banner('ABCD') == 'ABCD'

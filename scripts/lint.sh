22dbaa851f7a230662a4d73c5e981f706f6c0557
Timothy Crosley <timothy.crosley@gmail.com>
2022-12-30 11:20:38 -0800

    Bump to latest requirements / cruft template

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -6,5 +6,5 @@ poetry run mypy --ignore-missing-imports concentration/
 poetry run isort --check --diff concentration/ tests/
 poetry run black --check concentration/ tests/
 poetry run flake8 concentration/ tests/
-poetry run safety check -i 39462
+poetry run safety check -i 39462 -i 40291
 poetry run bandit -r concentration/

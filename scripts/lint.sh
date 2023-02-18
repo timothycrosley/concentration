eade68406cddb23eb9b05ff4a094da196bb77837
Timothy Crosley <timothy.crosley@gmail.com>
2023-02-17 22:49:43 -0800

    Update to pass lint checks

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -5,5 +5,5 @@ poetry run cruft check
 poetry run mypy --ignore-missing-imports concentration/
 poetry run black --check concentration/ tests/
 poetry run ruff concentration/ tests/
-poetry run safety check -i 39462 -i 40291
+poetry run safety check -i 39462 -i 40291 -i 52510
 poetry run bandit -r concentration/
